66e3b8fe13f329bca0a623fb9de8dba98783d066
Cedric De Vries <devriesced@gmail.com>
2025-11-05 00:16:04 +0000

    Changed app name

--- src/main/java/com/example/ISPStatDisplay/IspStatDisplayMongoDBApplication.java
+++ src/main/java/com/example/ISPStatDisplay/IspStatDisplayMongoDBApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class IspStatDisplayApplication {
+public class IspStatDisplayMongoDBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IspStatDisplayApplication.class, args);
+		SpringApplication.run(IspStatDisplayMongoDBApplication.class, args);
 	}
 
 }
